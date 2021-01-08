d8274d07b4d51399a569c5b4595dedf85a2ec905
ayresdiego <diego.ayres.br@gmail.com>
2021-01-08 17:56:42 +0100

    add-js

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+
+
+
+
+var btn_1 = document.querySelector(".project_1") // get the button to the a variable
+btn_1.addEventListener('click', function(url) { // add the event Listener to the variable
+    url = "https://www.facebook.com/"
+    url = "Project_race/index.html	"
+    var win = window.open(url, '_blank');
+    win.focus(); 
+
+    btn_1.classList.add("border_click");
+})
+
+
+var btn_2 = document.querySelector(".project_2") // get the button to the a variable
+btn_2.addEventListener('click', function(url) { // add the event Listener to the variable
+    url = "https://www.linkedin.com/feed/update/urn:li:activity:6728959018095652864/"
+    // url = "Project_2/index.html	"
+    var win = window.open(url, '_blank');
+    win.focus(); 
+
+    btn_2.classList.add("border_click");
+})
+
+
+tippy('.link', {
+    placement: 'top',
+})
